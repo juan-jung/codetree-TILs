056c00ca60891fab4a8b6a88de937c4a36fb70f7
juan-jung <118125595+juan-jung@users.noreply.github.com>
2024-04-13 02:15:43 +0900

    Title: 코드트리 오마카세, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240413/코드트리 오마카세/codetree-omakase.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int L = Integer.parseInt(st.nextToken());// 3 - 10억
+        int Q = Integer.parseInt(st.nextToken());// 1-10만
+        HashMap<String, ArrayList<int[]>> sushiMap = new HashMap<>();
+        HashMap<String, int[]> guestMap = new HashMap<>();
+        int total_sushi_cnt = 0;
+        int total_guest_cnt = 0;
+        for(int q=0;q<Q;q++) {
+            st = new StringTokenizer(br.readLine()," ");
+            int op = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            if(op==100) {
+                int x = Integer.parseInt(st.nextToken());
+                String name = st.nextToken();
+                ArrayList<int[]> info = sushiMap.getOrDefault(name, new ArrayList<int[]>());
+                info.add(new int[]{t,x});
+                sushiMap.put(name, info);
+                total_sushi_cnt++;
+            }
+            else if(op==200) {
+                int x = Integer.parseInt(st.nextToken());
+                String name = st.nextToken();
+                int n = Integer.parseInt(st.nextToken());
+                guestMap.put(name, new int[]{t,x,n});
+                total_guest_cnt++;
+            }
+            else if(op==300) {
+                //guest 순회하며 먹임
+                Set<String> keytSet = guestMap.keySet();
+                for(String key : keytSet) {
+                    int[] guestInfo = guestMap.get(key);
+                    int gtime = guestInfo[0];
+                    int gX = guestInfo[1];
+                    int leftSushi = guestInfo[2];
+                    if(leftSushi == 0) continue;
+
+                    ArrayList<int[]> sushis = sushiMap.get(key);
+                    Iterator iter = sushis.iterator();
+                    while(iter.hasNext()) {
+                        // t x
+                        int[] sushi = (int[])iter.next();
+                        //사람이먼저왔으면 x가 초기위치 , 아니면 시간차이만큼 이동한 자리가 초기위치
+                        int sushiStartX = (sushi[1] + (gtime > sushi[0] ? gtime-sushi[0] : 0))%L;
+                        int sushiMEndX = sushiStartX + t - (gtime > sushi[0] ? gtime : sushi[0]);
+                        if((gX>=sushiStartX && gX<=sushiMEndX) || gX<=(sushiMEndX%L) || sushiMEndX-sushiStartX >=L-1) {
+                            total_sushi_cnt--;
+                            iter.remove();
+                            leftSushi--;
+                        }
+                    }
+                    if(leftSushi == 0) total_guest_cnt--;
+                    guestMap.put(key,new int[]{gtime, gX, leftSushi});
+                }
+                System.out.println(total_guest_cnt + " " + total_sushi_cnt);
+            }
+        }
+    }
+}
